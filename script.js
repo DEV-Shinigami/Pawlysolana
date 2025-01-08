10331ac68c213b435ad500b64ead37c25e6f89e0
DEV Shinigami <cristiano.manuel.dias@gmail.com>
2025-01-08 14:11:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 // Seleciona os elementos necessários
 const nameElement = document.querySelector('.name');
-const portalLeft = document.querySelector('.portal-left');
-const portalRight = document.querySelector('.portal-right');
+const portal = document.querySelector('.portal');
 const siteContent = document.querySelector('.site-content');
 
 // Adiciona evento de clique no nome "PAWLY"
 nameElement.addEventListener('click', () => {
-    // Remove os portais com expansão
-    portalLeft.style.transition = 'transform 1s ease-in-out, opacity 1s';
-    portalRight.style.transition = 'transform 1s ease-in-out, opacity 1s';
-
-    portalLeft.style.transform = 'scale(10)';
-    portalRight.style.transform = 'scale(10)';
-
-    portalLeft.style.opacity = '0';
-    portalRight.style.opacity = '0';
+    // Remove o portal com expansão
+    portal.style.transition = 'transform 1s ease-in-out, opacity 1s';
+    portal.style.transform = 'scale(10)';
+    portal.style.opacity = '0';
 
     // Esconde o nome
     nameElement.style.opacity = '0';
